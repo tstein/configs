b8db6effc58f4530b24fd977560390c5fcb9f7bd
Ted Stein <karamarisan@gmail.com>
2012-11-26 21:06:45 -0600

    zsh: Automatically alias pip for the active venv.

--- zsh/python.zsh
+++ zsh/python.zsh
@@ -1,9 +1,22 @@
-# Fedora packages pip as pip-python. That's no fun.
+# Fedora packages pip as pip-python. Using the external /bin/which ensures we
+# get a real path and not a printout of an alias, even in nested zshes.
 if [ `whence pip-python` ]; then
-    alias pip=pip-python
+    local _PIP_PATH=`/bin/which pip-python`
+else
+    local _PIP_PATH=`/bin/which pip`
 fi
 
 if [ `whence virtualenvwrapper.sh` ]; then
     source =virtualenvwrapper.sh
 fi
+
+# At time of writing, pip will not detect an active venv and install to it.
+update_pip_alias() {
+    if [ "$VIRTUAL_ENV" ]; then
+        alias pip="$_PIP_PATH -E \"$VIRTUAL_ENV\""
+    else
+        alias pip="$_PIP_PATH"
+    fi
+}
+precmd_functions=($precmd_functions update_pip_alias)
 
