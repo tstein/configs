92aa6889751d73a96bd453683886e886908d7697
Ted Stein <karamarisan@gmail.com>
2013-01-23 16:03:49 -0600

    zsh: Update virtualenv config.

--- zsh/python.zsh
+++ zsh/python.zsh
@@ -6,17 +6,20 @@ else
     _PIP_PATH=`/bin/which pip`
 fi
 
+# Force virtualenv to use distribute instead of setuptools. This seems more
+# reliable.
+export VIRTUALENV_DISTRIBUTE="true"
 if [ `whence virtualenvwrapper.sh` ]; then
     source =virtualenvwrapper.sh
 fi
 
-# At time of writing, pip will not detect an active venv and install to it.
-update_pip_alias() {
-    if [ "$VIRTUAL_ENV" ]; then
-        alias pip="$_PIP_PATH -E \"$VIRTUAL_ENV\""
-    else
-        alias pip="$_PIP_PATH"
-    fi
-}
-precmd_functions=($precmd_functions update_pip_alias)
+# Since 1.1, pip knows what a virtualenv is. Remove this once that's widespread.
+#update_pip_alias() {
+#    if [ "$VIRTUAL_ENV" ]; then
+#        alias pip="$_PIP_PATH -E \"$VIRTUAL_ENV\""
+#    else
+#        alias pip="$_PIP_PATH"
+#    fi
+#}
+#precmd_functions=($precmd_functions update_pip_alias)
 
