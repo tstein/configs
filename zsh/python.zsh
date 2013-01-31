1e40648f85f9cde5f71753db98f3bf2b81dff8f9
Ted Stein <me@tedstein.net>
2013-01-30 21:01:59 -0600

    zsh: Fix and improve python.zsh.

--- zsh/python.zsh
+++ zsh/python.zsh
@@ -1,3 +1,5 @@
+export PYTHONPATH="~/.local/lib/python2.7/site-packages":$PYTHONPATH
+
 # Force virtualenv to use distribute instead of setuptools. This seems more
 # reliable.
 export VIRTUALENV_DISTRIBUTE="true"
@@ -5,22 +7,8 @@ if [ `whence virtualenvwrapper.sh` ]; then
     source =virtualenvwrapper.sh
 fi
 
-# Since 1.1, pip knows what a virtualenv is. Delete from here down once that's
-# widespread.
-#
-# Fedora packages pip as pip-python. Using the external /bin/which ensures we
-# get a real path and not a printout of an alias, even in nested zshes.
-#if [ `whence pip-python` ]; then
-#    _PIP_PATH=`/bin/which pip-python`
-#else
-#    _PIP_PATH=`/bin/which pip`
-#fi
-#update_pip_alias() {
-#    if [ "$VIRTUAL_ENV" ]; then
-#        alias pip="$_PIP_PATH -E \"$VIRTUAL_ENV\""
-#    else
-#        alias pip="$_PIP_PATH"
-#    fi
-#}
-#precmd_functions=($precmd_functions update_pip_alias)
+# Fedora packages pip as pip-python.
+if [ `whence pip-python` ]; then
+    alias pip=pip-python
+fi
 
