931d65de7a4c29fdb83a02e872cd5f98ff218c61
Ted Stein <me@tedstein.net>
2013-02-01 18:09:27 -0600

    zsh: Use a symlink instead of an alias for Fedora's pip-python.

--- zsh/python.zsh
+++ zsh/python.zsh
@@ -5,8 +5,11 @@ if [ `whence virtualenvwrapper.sh` ]; then
     source =virtualenvwrapper.sh
 fi
 
-# Fedora packages pip as pip-python.
-if [ `whence pip-python` ]; then
-    alias pip=pip-python
+# Fedora packages pip as pip-python. However, virtualenv will install it as
+# `pip`. Aliasing pip to pip-python will mask the venv's pip.
+if [ =pip-python ]; then
+    if [ ! -e ~/.local/bin/pip ]; then
+        ln -s =pip-python ~/.local/bin/pip
+    fi
 fi
 
