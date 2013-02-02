09aed18beda2a650a094084bacba0617f8c082a6
Ted Stein <me@tedstein.net>
2013-02-01 18:36:23 -0600

    zsh: Update automat to use .local.

--- zsh/00.local.zsh
+++ zsh/00.local.zsh
@@ -3,7 +3,7 @@
 if [ ! -d ~/.local ]; then
     mkdir ~/.local
 fi
-SUBDIRS=(bin lib share tmp)
+SUBDIRS=(bin include lib share tmp)
 for DIR in $SUBDIRS; do
     if [ ! -d ~/.local/$DIR ]; then
         mkdir ~/.local/$DIR
