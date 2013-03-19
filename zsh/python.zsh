beb61de0f11684960739670e7cb2e00f2b6bb8e0
Ted Stein <me@tedstein.net>
2013-03-19 15:45:34 -0500

    zsh: Use a better test for pip-python.

--- zsh/python.zsh
+++ zsh/python.zsh
@@ -7,7 +7,7 @@ fi
 
 # Fedora packages pip as pip-python. However, virtualenv will install it as
 # `pip`. Aliasing pip to pip-python will mask the venv's pip.
-if [ =pip-python ]; then
+if whence pip-python >/dev/null; then
     if [ ! -e ~/.local/bin/pip ]; then
         ln -s =pip-python ~/.local/bin/pip
     fi
