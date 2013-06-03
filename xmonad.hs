8bf865b9a2a0c6a78d41851cedc94f3d4514cc77
Ted Stein <me@tedstein.net>
2013-06-03 13:34:56 -0500

    xmonad: Prettier colors.

--- xmonad.hs
+++ xmonad.hs
@@ -10,14 +10,19 @@ import XMonad.Layout.NoBorders
 import XMonad.Layout.PerWorkspace
 import XMonad.Layout.Reflect
 import XMonad.Util.EZConfig
+import XMonad.Util.Run
 import Data.Ratio ((%))
+import Text.Printf
 import qualified XMonad.StackSet as W
 import qualified Data.Map        as M
 
-myModMask = mod4Mask
+
+myModMask = mod4Mask    -- Super.
 
 myWorkspaces = ["1", "2", "3", "4", "5", "6", "7", "8", "9"]
 
+-- That monster at the bottom allows moving windows with <M-arrows> and resizing
+-- them with <M-S-arrows>.
 keyBindings = [ ("M-p",          spawn "exec `yeganesh -x`")
               , ("M-S-k",        spawn "keepassx")
               , ("M-S-l",        spawn "xscreensaver-command -lock")
@@ -36,10 +41,12 @@ keyBindings = [ ("M-p",          spawn "exec `yeganesh -x`")
                   , (c, x) <- zip ["", "C-"] [20, 2]
               ]
 
+-- Change workspaces by rolling the mousewheel.
 myMouseBindings = [ ((mod4Mask, button4), (\_ -> nextWS))
                   , ((mod4Mask, button5), (\_ -> prevWS))
                   ]
 
+-- Give the first desktop a layout that shows a small buddy list.
 myLayoutHook = onWorkspace "1" pidgin
              $ full ||| tiled ||| htiled
                where
@@ -52,37 +59,56 @@ myLayoutHook = onWorkspace "1" pidgin
                    ratio   = 1/2
                    delta   = 3/100
 
+-- Windows are floated by default.
 myManageHook = composeAll . concat $
     [ [ isFullscreen      --> doFullFloat ]
-    , [ className =? name --> doFloat       | name <- floatByClass ]
     , [ className =? name --> doIgnore      | name <- ignoreByClass ]
     , [ resource  =? name --> doIgnore      | name <- ignoreByResource ]
     ]
     where
-       floatByClass = [ "MPlayer", "Gimp", "vlc" ]
        ignoreByClass = [ "xfce4-notifyd", "Xfce4-notifyd" ]
        ignoreByResource = [ "desktop_window" ]
 
-avoidMaster :: W.StackSet i l a s sd -> W.StackSet i l a s sd
-avoidMaster = W.modify' $ \c -> case c of
-     W.Stack t [] (r:rs) -> W.Stack t [r] rs
-     otherwise           -> c
 
 ------------------------------------------------------------------------
-main = xmonad =<< xmobar myConfig
-myConfig = defaultConfig {
-        modMask            = myModMask,
-        focusFollowsMouse  = False,
-        borderWidth        = 1,
-        terminal           = "terminator",
-        workspaces         = myWorkspaces,
-        normalBorderColor  = "#ddddff",
-        focusedBorderColor = "#0000dd",
-        layoutHook         = avoidStruts $ myLayoutHook,
-        manageHook         = manageDocks <+> myManageHook <+> doFloat
-    }
-    `additionalKeysP`
-    keyBindings
-    `additionalMouseBindings`
-    myMouseBindings
+colorFunc = xmobarColor
+textBg = "black"
+
+formatFunc = \fg -> colorFunc fg textBg
+formatCurrent = formatFunc "#00ffff"           -- "cyan"
+formatVisible = formatFunc "#006666"           -- darker cyan
+formatHidden = formatFunc "#aaaaaa"            -- "grey"
+formatHiddenNoWindows = formatFunc "#444444"   -- darker grey
+formatUrgent = formatFunc "#dd0000"            -- slightly darker red
+formatTitle = formatCurrent
+
+myPP h = xmobarPP {
+           ppOutput = hPutStrLn h,
+           ppCurrent = formatCurrent,
+           ppVisible = formatVisible,
+           ppTitle = formatTitle,
+           ppHidden = formatHidden,
+           ppHiddenNoWindows = formatHiddenNoWindows,
+           ppUrgent = formatUrgent,
+           ppSep = " | "
+       }
+
+main = do
+    h <- spawnPipe "xmobar"
+    xmonad $ defaultConfig {
+                 modMask            = myModMask,
+                 focusFollowsMouse  = False,
+                 borderWidth        = 1,
+                 terminal           = "terminator",
+                 workspaces         = myWorkspaces,
+                 normalBorderColor  = "#ddddff",
+                 focusedBorderColor = "#0000dd",
+                 layoutHook         = avoidStruts $ myLayoutHook,
+                 manageHook         = manageDocks <+> myManageHook <+> doFloat,
+                 logHook            = dynamicLogWithPP $ myPP h
+             }
+             `additionalKeysP`
+             keyBindings
+             `additionalMouseBindings`
+             myMouseBindings
 
