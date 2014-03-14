af2bb226739215ea4e2c732d8e641e5e24bc2561
Ted Stein <tedstein@amazon.com>
2014-03-14 11:51:26 -0700

    zsh/functools: Add `in`.

--- zsh/05functools.zsh
+++ zsh/05functools.zsh
@@ -1,7 +1,7 @@
 # Higher-order functions for zsh.
 
 function filter() {
-  if (( $# == 0 )); then
+  if (( $# < 2 )); then
     print 'usage: filter COMMAND [ARG...]'
     print 'Apply COMMAND to each ARG and print the ARGs for which COMMAND returned 0, space-separated.'
     return
@@ -16,8 +16,28 @@ function filter() {
   print
 }
 
+function in() {
+  if (( $# < 2 )); then
+    print 'usage: in DIR COMMAND [ARG...]'
+    print 'Run COMMAND with ARGS in DIR.'
+    return
+  fi
+  local DIR=`readlink -m $@[1]`
+  if [ ! -d $DIR ]; then
+    print "in: no such directory: $DIR"
+    return 255
+  fi
+  pushd $DIR
+  eval "$@[2,-1]"
+  local RETVAL=$?
+  popd
+  return $RETVAL
+}
+
+
+
 function map() {
-  if (( $# == 0 )); then
+  if (( $# < 2 )); then
     print 'usage: map COMMAND [ARG...]'
     print 'Apply COMMAND to each ARG in sequence.'
     return
