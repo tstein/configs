89cfb64372f37755c6e31930b9339abddc58d3ba
Ted Stein <tedstein@amazon.com>
2014-03-19 16:32:07 -0700

    zsh: Improve functools with _ substitution.

--- zsh/05functools.zsh
+++ zsh/05functools.zsh
@@ -1,19 +1,29 @@
 # Higher-order functions for zsh.
 
-function filter() {
-  if (( $# < 2 )); then
-    print 'usage: filter COMMAND [ARG...]'
-    print 'Apply COMMAND to each ARG and print the ARGs for which COMMAND returned 0, space-separated.'
+function apply1() {
+  if (( $# != 2 )); then
+    print 'usage: apply1 COMMAND ARG'
+    print 'Apply COMMAND to ARG. If COMMAND contains tokens consisting of a'
+    print '  single underscore, ARG will replace it. Otherwise, ARG will be the'
+    print '  last argument passed to COMMAND.'
     return
   fi
   local COMMAND=$@[1]
-  for ARG in $@[2,-1]; do
-    eval "$COMMAND $ARG >/dev/null 2>/dev/null"
-    if (( $? == 0 )); then
-      print -n "$ARG "
+  local ARG=$@[2]
+  local EXEC=''
+  local APPENDARG='yep'
+  for TOKEN in "${(s/ /)COMMAND}"; do
+    if [[ $TOKEN == '_' ]]; then
+      EXEC="$EXEC $ARG"
+      APPENDARG='nope'
+    else
+      EXEC="$EXEC $TOKEN "
     fi
   done
-  print
+  if [[ $APPENDARG == 'yep' ]]; then
+    EXEC="$EXEC $ARG"
+  fi
+  eval $EXEC
 }
 
 function in() {
@@ -28,13 +38,12 @@ function in() {
     return 255
   fi
   pushd $DIR
+  print "in: $DIR"
   eval "$@[2,-1]"
   local RETVAL=$?
   popd
   return $RETVAL
 }
-
-
 
 function map() {
   if (( $# < 2 )); then
@@ -44,6 +53,22 @@ function map() {
   fi
   local COMMAND=$@[1]
   for ARG in $@[2,-1]; do
-    eval "$COMMAND $ARG"
+    apply1 $COMMAND $ARG
   done
+}
+
+function filter() {
+  if (( $# < 2 )); then
+    print 'usage: filter COMMAND [ARG...]'
+    print 'Apply COMMAND to each ARG and print the ARGs for which COMMAND returned 0, space-separated.'
+    return
+  fi
+  local COMMAND=$@[1]
+  for ARG in $@[2,-1]; do
+    apply1 $COMMAND $ARG >/dev/null 2>/dev/null
+    if (( $? == 0 )); then
+      print -n "$ARG "
+    fi
+  done
+  print
 }
