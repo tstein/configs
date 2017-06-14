cefe698a0d1a75ccdb4f789bf07cb1a97cc52ed0
Ted Stein <me@tedstein.net>
2017-06-14 13:29:28 -0700

    hammerspoon: Remove music syncing.

--- hammerspoon.lua
+++ hammerspoon.lua
@@ -22,53 +22,6 @@ hs.hotkey.bind({"cmd", "option"}, "L", function()
   hs.caffeinate.lockScreen()
 end)
 
--- Auto-sync music when on home wifi.
-local musicNotification
-function withdrawMusicNotification()
-    if musicNotification ~= nil then
-        musicNotification:withdraw()
-        musicNotification = nil
-    end
-end
-
-function wifiChanged(watcher, message, interface)
-    function syncOutput(task, stdOut, stdErr)
-        if stdOut ~= "" then print(stdOut) end
-        if stdErr ~= "" then print(stdErr) end
-        return true
-    end
-
-    function syncComplete(exitCode, stdOut, stdErr)
-        withdrawMusicNotification()
-        if exitCode == 0 then
-            musicNotification = hs.notify.show("music synced", "", "")
-        else
-            musicNotification = hs.notify.show(
-                "error syncing music",
-                "Check the hs console for details.",
-                "")
-        end
-    end
-
-    if message ~= "SSIDChange" then return end
-
-    local newSSID = hs.wifi.currentNetwork(interface)
-    if newSSID == "armory" or newSSID == "armory legacy" then
-        if hs.fs.volume.allVolumes()["/Volumes/bonus"] ~= nil then
-            withdrawMusicNotification()
-            musicNotification = hs.notify.show("syncing music...", "", "")
-            local syncTask = hs.task.new(
-                "/usr/local/bin/zsh",
-                syncComplete,
-                syncOutput,
-                {"-ic", "syncmusic"})
-            syncTask:start()
-        end
-    end
-end
-local wifiWatcher = hs.wifi.watcher.new(wifiChanged)
-wifiWatcher:start()
-
 -- Auto-reload this file when it's saved.
 function reloadConfig(files)
     doReload = false
