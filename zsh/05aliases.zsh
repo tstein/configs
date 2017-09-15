2e8511df429434041eddd239377f1d3adb968418
Ted Stein <me@tedstein.net>
2017-09-15 00:01:25 -0700

    zsh: Add pdfcat alias.

--- zsh/05aliases.zsh
+++ zsh/05aliases.zsh
@@ -9,10 +9,11 @@ alias -- -='cd -'
 alias absname='readlink -m'
 alias dict='dict -d wn'
 alias no='yes n'
+alias pdfcat='gs -dBATCH -dNOPAUSE -q -sDEVICE=pdfwrite -sOutputFile=out.pdf'
+alias rezsh='source ~/.zshrc'
 if [[ `get_prop OS` == 'Linux' ]]; then
     alias open='xdg-open'
 fi
-alias rezsh='source ~/.zshrc'
 
 # Option-setters:
 alias bc='bc -l'
---
+++ zsh/20go.zsh
@@ -0,0 +1,2 @@
+export GOPATH=~/code/go
+export PATH=$GOPATH/bin:$PATH
