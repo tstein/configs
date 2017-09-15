0c34fde9e3633755cb4d174ebb66b5e5bde34358
Ted Stein <me@tedstein.net>
2017-09-14 23:54:44 -0700

    zsh: Fedora doesn't package pip as pip-python anymore.

--- zsh/20python.zsh
+++ zsh/20python.zsh
@@ -1,12 +1,3 @@
 if [ `whence virtualenvwrapper.sh` ]; then
     source =virtualenvwrapper.sh
 fi
-
-# Fedora packages pip as pip-python. However, virtualenv will install it as
-# `pip`. Aliasing pip to pip-python will mask the venv's pip.
-if whence pip-python >/dev/null; then
-    if [ ! -e ~/.local/bin/pip ]; then
-        ln -s =pip-python ~/.local/bin/pip
-    fi
-fi
-
