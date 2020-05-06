c94ac281e845081893da6ad91a7e0c2979271898
Ted Stein <me@tedstein.net>
2020-05-06 00:16:28 -0700

    zsh: Fix a security issue. Wow.
    Commit b31ccb1e2813014c3039aad259f3b4d73d02105a, from 2014/01/30, added
    some dirs under .local to LD_LIBRARY_PATH, to make .local installs of
    libraries more usable. Turns out ld.so has the bizarre behavior that an
    empty element, such as you get when $LD_LIBRARY_PATH is empty and you
    set it to "/something:$LD_LIBRARY_PATH", is treated as meaning the
    current directory.
    
    Noticed this while investigating extreme slowness when using sshfs over a
    connection with 250 ms of organic latency. Thought it was an issue with
    sshfs serializing stat calls or something, turns out it was ld.so stat'ing
    nonexistent libraries in . over and over.

--- zsh/00.local.zsh
+++ zsh/00.local.zsh
@@ -11,4 +11,12 @@ for DIR in $SUBDIRS; do
 done
 unset SUBDIRS
 
-export LD_LIBRARY_PATH=~/.local/lib64:~/.local/lib:$LD_LIBRARY_PATH
+# For some unfathomable reason, ld.so considers paths with empty elements in
+# LD_LIBRARY_PATH like `/a:` or `/a::/b`, to include the current directory. This
+# is very bad.
+if [[ -z "${LD_LIBRARY_PATH// }" ]]; then
+    export LD_LIBRARY_PATH=~/.local/lib64:~/.local/lib
+else
+    export LD_LIBRARY_PATH=~/.local/lib64:~/.local/lib:$LD_LIBRARY_PATH
+fi
+
