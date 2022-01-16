ba99e73edabe19c350dfc3753b88a2d96946278c
Ted Stein <me@tedstein.net>
2022-01-16 01:32:14 -0800

    setup.zsh: Rework and update.

--- setup.zsh
+++ setup.zsh
@@ -1,31 +1,29 @@
 #!/bin/zsh
 
-CONFIGS=`pwd`
-cd ~
-ln -s "$CONFIGS/dir_colors" .dir_colors
-ln -s "$CONFIGS/tmux.conf" .tmux.conf
-ln -s "$CONFIGS/vim" .vim
-ln -s "$CONFIGS/vimrc" .vimrc
-ln -s "$CONFIGS/zsh" .zsh
-ln -s "$CONFIGS/zshrc" .zshrc
-if [[ "$USER" == "ted" ]]; then
-    ln -s "$CONFIGS/gitconfig" .gitconfig
-else
-    print "you may not be ted. skipping gitconfig."
-fi
-
-mkdir -p ~/.local/bin
-# vim expects this to exist
-mkdir -p ~/.local/tmp
-
-mkdir -p ~/.config/nvim ~/.local/share/nvim/site/pack
-ln -s "$CONFIGS/nvim/init.vim" ~/.config/nvim/init.vim
-ln -s "$CONFIGS/nvim/plugins" ~/.local/share/nvim/site/pack/plugins
-
-ln -s "$CONFIGS/bin/mtmux" ~/.local/bin/
-
+CONFIGS=`dirname $0`
 cd $CONFIGS
 git submodule update --init --recursive
+
+mkdir -p ~/.config/nvim
+mkdir -p ~/.local/{bin,tmp}
+
+ln -ns "$CONFIGS/bin/mtmux" ~/.local/bin/mtmux
+ln -ns "$CONFIGS/dir_colors" ~/.dir_colors
+ln -ns "$CONFIGS/nvim/init.vim" ~/.config/nvim/init.vim
+ln -ns "$CONFIGS/tmux.conf" ~/.tmux.conf
+ln -ns "$CONFIGS/vim" ~/.vim
+ln -ns "$CONFIGS/vimrc" ~/.vimrc
+ln -ns "$CONFIGS/zsh" ~/.zsh
+ln -ns "$CONFIGS/zshrc" ~/.zshrc
+if [[ "$USER" == "ted" ]]; then
+    ln -ns "$CONFIGS/gitconfig" ~/.gitconfig
+else
+    print "skipping gitconfig because you may not be ted"
+fi
+
 vim +BundleInstall +qa
+if [ which nvim >/dev/null 2>/dev/null ]; then
+  nvim +PlugInstall
+fi
 
 exec zsh
