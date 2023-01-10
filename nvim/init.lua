64f973863665c772d75365fd742444cc0328f954
Ted Stein <me@tedstein.net>
2023-01-10 00:35:36 -0800

    nvim: Move packer stuff into a more obviously-named and -special file.

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,4 +1,4 @@
-require "plugins"
+require "00_packer"
 
 local modules_dir = vim.loop.fs_scandir(vim.fn.stdpath("config") .. "/lua/")
 for module in function() return vim.loop.fs_scandir_next(modules_dir) end do
