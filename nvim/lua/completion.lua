2a1a717c9ba828156a41f430788ac2007ab0daa0
Ted Stein <me@tedstein.net>
2023-01-10 00:02:21 -0800

    nvim: Boil down and fix rust analyzer config.

--- nvim/lua/completion.lua
+++ nvim/lua/completion.lua
@@ -1,46 +1,6 @@
 vim.cmd [[ let g:deoplete#enable_at_startup = 1 ]]
 vim.opt.completeopt = "menuone,noinsert,noselect"
 
--- lsp
-local nvim_lsp = require'lspconfig'.rust_analyzer.setup {
-  single_file_support = true,
-}
-
-local opts = {
-    tools = {
-        autoSetHints = true,
-        hover_with_actions = true,
-        runnables = {
-            use_telescope = true
-        },
-        inlay_hints = {
-            show_parameter_hints = false,
-            parameter_hints_prefix = "",
-            other_hints_prefix = "",
-        },
-    },
-
-    -- all the opts to send to nvim-lspconfig
-    -- these override the defaults set by rust-tools.nvim
-    -- see https://github.com/neovim/nvim-lspconfig/blob/master/CONFIG.md#rust_analyzer
-    server = {
-        -- on_attach is a callback called when the language server attachs to the buffer
-        -- on_attach = on_attach,
-        settings = {
-            -- to enable rust-analyzer settings visit:
-            -- https://github.com/rust-analyzer/rust-analyzer/blob/master/docs/user/generated_config.adoc
-            ["rust-analyzer"] = {
-                -- enable clippy on save
-                checkOnSave = {
-                    command = "clippy"
-                },
-            }
-        }
-    },
-}
-
-require('rust-tools').setup(opts)
-
 -- cmp
 local cmp = require'cmp'
 cmp.setup({
--- nvim/lua/rust.lua
+++ nvim/lua/rust.lua
@@ -1,1 +1,14 @@
-require('rust-tools').setup({})
+local rt = require("rust-tools")
+rt.setup({
+  tools = {
+    hover_actions = {
+      auto_focus = true
+    },
+  },
+  server = {
+    on_attach = function(_, bufnr)
+      vim.keymap.set("n", "<C-space>", rt.hover_actions.hover_actions, { buffer = bufnr })
+      vim.keymap.set("n", "<Leader>a", rt.code_action_group.code_action_group, { buffer = bufnr })
+    end,
+  },
+})
