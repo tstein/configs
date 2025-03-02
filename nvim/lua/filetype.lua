8648aeeb1362f71be6ddda131a4f46a05676c4dd
Ted Stein <me@tedstein.net>
2025-03-02 10:05:23 -0800

    nvim: Don't wrap on fstab or playlists.

--- nvim/lua/filetype.lua
+++ nvim/lua/filetype.lua
@@ -5,6 +5,12 @@ vim.api.nvim_create_autocmd("FileType", {
   command = "setlocal ts=2 sts=2 sw=2"
 })
 
+-- Actively unhelpful to wrap some filetypes.
+vim.api.nvim_create_autocmd("FileType", {
+  pattern = {"fstab", "hlsplaylist"},
+  command = "setlocal tw=0"
+})
+
 vim.api.nvim_create_autocmd("FileType", {
   pattern = "python",
   command = "setlocal tw=88"  -- black-style and ruff's default
