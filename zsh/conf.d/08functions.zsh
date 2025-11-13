518418181aa6fd27371f3140a5ea56f7de640f73
Ted Stein <me@tedstein.net>
2025-11-12 16:24:40 -0800

    zsh: Add do-and-tell function.

--- zsh/conf.d/08functions.zsh
+++ zsh/conf.d/08functions.zsh
@@ -1,3 +1,21 @@
+function do-and-tell() {
+  start=$(cut -d ' ' -f 1 /proc/uptime)
+  eval $@
+  retcode=$?
+  duration=$(($(cut -d ' ' -f 1 /proc/uptime) - $start))
+  # No need to create unread messages if it returns immediately.
+  if [[ $duration > 1.0 ]]; then
+    duration_s=$(printf '%.1fs' $duration)
+    # Assume tell is set up.
+    if [[ $retcode == 0 ]]; then
+      tell ted "${HOST}: \`$*\` completed in ${duration_s}"
+    else
+      tell ted "${HOST}: \`$*\` returned ${retcode} in ${duration_s}"
+    fi
+  fi
+  return retcode
+}
+
 function maclookup() {
   # ssh into the router and retrieve all MAC addresses known for the given
   # hostname.
