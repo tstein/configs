8173d33c19295eb95713dbee0bbadf233aab3e7f
Ted Stein <me@tedstein.net>
2025-12-28 17:00:59 -0800

    nvim: Add filetypes for systemd things, and remove the unnecessary python one

--- nvim/lua/filetype.lua
+++ nvim/lua/filetype.lua
@@ -1,5 +1,15 @@
 -- Per-filetype settings.
+vim.filetype.add({
+  -- systemd units and defs aren't recognized by default.
+  extension = {
+    container = "systemd",
+    service = "systemd",
+    timer = "systemd",
+    volume = "systemd",
+  },
+})
 
+-- two-space soft tabs by default
 vim.api.nvim_create_autocmd("FileType", {
   pattern = "*",
   command = "setlocal ts=2 sts=2 sw=2"
@@ -9,9 +19,4 @@ vim.api.nvim_create_autocmd("FileType", {
 vim.api.nvim_create_autocmd("FileType", {
   pattern = {"fstab", "hlsplaylist"},
   command = "setlocal tw=0"
-})
-
-vim.api.nvim_create_autocmd("FileType", {
-  pattern = "python",
-  command = "setlocal tw=88"  -- black-style and ruff's default
 })
